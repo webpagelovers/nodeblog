7827681e400e48473a5d1158883e22ed4b0605e2
webpagelovers <1546786921@qq.com>
2018-11-20 01:31:40 +0800

    a a

--- autoBuild.js
+++ autoBuild.js
@@ -1,38 +1,32 @@
+// autoBuild.js
 var http = require('http')
-var createHandler = require('github-webhook-handler')
-var handler = createHandler({path: '/nodeblog', secret: '123456'})
-
-// 上面的 secret 保持和 GitHub 后台设置的一致
-
-function run_cmd(cmd, args, callback) {
-    var spawn = require('child_process').spawn;
-    var child = spawn(cmd, args);
-    var resp = "";
-
-    child.stdout.on('data', function (buffer) {
-        resp += buffer.toString();
-    });
-    child.stdout.on('end', function () {
-        callback(resp)
-    });
-}
-
+var spawn = require('child_process').spawn
+var createHandler = require('github-Webhooks-handler')
+var handler = createHandler({ path: '/pushCode', secret: '' }) // 在代码仓库的 Webhooks 选项处配置
 http.createServer(function (req, res) {
     handler(req, res, function (err) {
-        res.statusCode = 404
+        res.statusCode = 404;
         res.end('no such location')
     })
-}).listen(80)
+}).listen(7777)
 
 handler.on('error', function (err) {
     console.error('Error:', err.message)
 })
 
+// 监听 push 事件
 handler.on('push', function (event) {
     console.log('Received a push event for %s to %s',
         event.payload.repository.name,
-        event.payload.ref);
-    run_cmd('sh', ['./autoBuild.sh', event.payload.repository.name], function (text) {
-        console.log(text)
-    });
+        event.payload.ref)
+    rumCommand('sh', ['./autoBuild.sh'], function( txt ) { // 执行 autoBuild.sh 脚本文件
+        console.log(txt)
+    })
 })
+
+function rumCommand( cmd, args, callback ) {
+    var child = spawn( cmd, args )
+    var response = ''
+    child.stdout.on('data', function( buffer ){ response += buffer.toString(); })
+    child.stdout.on('end', function(){ callback( response ) })
+}
